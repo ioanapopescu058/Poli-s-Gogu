4d3841315f526e2c2b16ceeebd42b63c5e4d85e9
Ioana <ioana.popescu_5@yahoo.com>
2017-11-19 17:48:11 +0200

    First Commit

--- Gogu/src/gogu/Gogu.java
+++ Gogu/src/gogu/Gogu.java
@@ -17,6 +17,7 @@ public class Gogu {
     public static void main(String[] args) {
         // TODO code application logic here
         //cvbcbvsdfsdfasdad
+        System.out.println("Hello");
     }
     
 }
